737172fd22f4a68f4c3e977d41f30458a9efb0e8
Rohail Sheikh <86629893+Antendis@users.noreply.github.com>
2025-07-11 03:52:22 +0100

    Update DHKE.py

--- DHKE.py
+++ DHKE.py
@@ -12,5 +12,21 @@ person_1_secret = random.randint(1, 20)
 person_2_secret = random.randint(1, 20) 
 
 print(f"person 1's secret: {person_1_secret}")
-print(f"person 2's secret: {person_2_secret}")
+print(f"person 2's secret: {person_2_secret}\n")
 
+person_1_public = base ** person_1_secret % prime
+person_2_public = base ** person_2_secret % prime
+
+print(f"person 1's public key: {person_1_public}")
+print(f"person 2's public key: {person_2_public}\n")
+
+shared_secret = person_2_public ** person_1_secret % prime
+shared_secret_check = person_1_public ** person_2_secret % prime
+
+print(f"Shared secret: {shared_secret}")
+print(f"Shared secret check: {shared_secret_check}\n")
+
+if shared_secret == shared_secret_check:
+    print("Shared secret successfully established!")
+else:
+    print("Shared secret mismatch, something went wrong!")
