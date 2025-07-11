58bec65262e5245ff4a08a7e51577c64bd7d511d
Rohail Sheikh <86629893+Antendis@users.noreply.github.com>
2025-07-11 03:46:57 +0100

    Update DHKE.py

--- DHKE.py
+++ DHKE.py
@@ -1,4 +1,16 @@
 from Crypto.Util import number
+import random
 
 prime = number.getPrime(64) # Gets a random 64-bit prime number
 print(f"Agreed upon prime: {prime}")
+
+base_choices = [2, 3, 5]
+base = random.choice(base_choices)  # Randomly choose a base from the list
+print(f"Agreed upon base: {base}\n")
+
+person_1_secret = random.randint(1, 20) 
+person_2_secret = random.randint(1, 20) 
+
+print(f"person 1's secret: {person_1_secret}")
+print(f"person 2's secret: {person_2_secret}")
+
